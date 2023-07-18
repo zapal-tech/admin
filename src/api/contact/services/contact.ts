bb62a503b5fb26e50c55215db21bdfd6cfbe4689
Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
2023-07-18 22:34:29 +0300

    fix: telegram notifications

--- src/api/contact/services/contact.ts
+++ src/api/contact/services/contact.ts
@@ -1,4 +1,5 @@
 import { factories } from '@strapi/strapi';
+import { Bot } from 'grammy';
 
 type Contact = {
   id: number;
@@ -11,18 +12,27 @@ type Contact = {
   updatedAt: string;
 };
 
-export default factories.createCoreService('api::contact.contact', ({ strapi }) => ({
+export default factories.createCoreService('api::contact.contact', () => ({
   async create(params) {
     const result = (await super.create(params)) as Contact;
 
-    const { firstName, lastName, email, company, message } = result;
-
-    const notificationText = `Hi, Zapal!\n\n${firstName} ${lastName} from ${company} sent you a message:\n${message}\n\nYou can reply to ${email}`;
-
     try {
-      await strapi.service('api::notification.notification').sendTelegramNotification(notificationText);
+      const { firstName, lastName, email, company, message } = result;
+
+      const notificationText = `Hi, Zapal!\n\n${firstName} ${lastName} from ${company} sent you a message:\n${message}\n\nYou can reply to ${email}`;
+
+      const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
+
+      await bot.start();
+
+      await bot.api.sendMessage(process.env.TELEGRAM_CHAT_ID, notificationText);
+
+      console.log('Telegram notification sent!');
+
+      await bot.stop();
     } catch (error) {
       console.log('Error calling notification service (telegram), but contact was created.');
+      console.error(error);
     }
 
     return result;
