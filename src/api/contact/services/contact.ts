4ac083ad6fe1183460a8ae5ce4744ae16d04e8e2
Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
2023-07-18 21:06:12 +0300

    fix: wrong service implementation

--- src/api/contact/services/contact.ts
+++ src/api/contact/services/contact.ts
@@ -1,3 +1,17 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::contact.contact');
+export default factories.createCoreService('api::contact.contact', ({ strapi }) => ({
+  async create(params) {
+    const result = await super.create(params);
+
+    console.log(result);
+
+    try {
+      await strapi.service('api::notification.notification').sendTelegramNotification(result?.toString?.());
+    } catch (error) {
+      console.error(error);
+    }
+
+    return result;
+  },
+}));
--- src/api/module/services/contact-form.ts
+++ src/api/module/services/contact-form.ts
@@ -1,17 +1,3 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::module.contact-form', ({ strapi }) => ({
-  async create(params) {
-    const result = await super.create(params);
-
-    console.log(result);
-
-    try {
-      await strapi.service('api::notification.notification').sendTelegramNotification(result?.toString?.());
-    } catch (error) {
-      console.error(error);
-    }
-
-    return result;
-  },
-}));
+export default factories.createCoreService('api::module.contact-form');
