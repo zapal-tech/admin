44c408615dcafa0a0145ceb975a010b5158d159c
Bohdan Kucheriavyi <bohdan.kucheriavyi@zapal.tech>
2023-07-23 19:34:22 +0300

    fix: article population

--- src/api/article/services/article.ts
+++ src/api/article/services/article.ts
@@ -1,11 +1,13 @@
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreService('api::article.article', ({ strapi }) => ({
-  async findOne(slug: string | number, params) {
+  async findOne(slug: string | number) {
     const isId = Number.isInteger(parseInt(`${slug}`));
-    const entity = await strapi.db
-      .query('api::article.article')
-      .findOne({ where: isId ? { $or: [{ slug }, { id: slug }] } : { slug }, populate: params.populate });
+
+    const entity = await strapi.db.query('api::article.article').findOne({
+      where: isId ? { $or: [{ slug }, { id: slug }] } : { slug },
+      populate: ['cover', 'author', 'tag', 'content', 'seo'],
+    });
 
     return entity;
   },
